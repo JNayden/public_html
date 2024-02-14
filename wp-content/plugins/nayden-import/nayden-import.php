b1d229e329757f995ae8adcddd712b07075df159
JNayden <56478030+JNayden@users.noreply.github.com>
2024-02-14 08:47:00 +0200

    receiving data from js form and create a product N: 1) recevice_js_data() 2) delete_products() 3) my_plugin_scripts() M: r:125+: Abs path, so can't bother fetch request
    require wp-load.php, so i can use wp function

--- wp-content/plugins/nayden-import/nayden-import.php
+++ wp-content/plugins/nayden-import/nayden-import.php
@@ -3,12 +3,14 @@
  * Plugin Name: Nayden Import
  * Description: desc
  * Author: Nayden
- * Author URI: https://gorose.bg
+ * Author URI: https://example.com
  * Version: 1.0.0
  * Text Domain: nayden-import
  * 
  */
 // Add a menu item to the WordPress admin menu
+
+require_once(__DIR__.'../../../../wp-load.php');
 function product_importer_menu() {
     add_menu_page( 'Nayden Product Importer', 'Nayden Product Importer', 'manage_options', 'nayden-product-importer', 'product_importer_page' );
 }
@@ -62,11 +64,7 @@ function process_product_import() {
             //     // Add the current line to the array
             //     $allLines[] = $line;
             // }
-            
-            // echo "All lines: " . sizeof($allLines);
-            // Close the file handle
-            fclose($handle);
-    
+
             // $column_headers now contains an array of column headers from the CSV file
             // You can use this array to dynamically populate options in your HTML select element
             // For example, you can encode this array as JSON and pass it to JavaScript
@@ -80,38 +78,71 @@ function process_product_import() {
                     var firstRow = <?php echo $first_row_json; ?>;
             </script>
             <?php
+            my_plugin_scripts();
         } else {
             // Error opening file
             echo "Error opening the CSV file.";
         }
-        ?>
-        
-        <script src="..\wp-content\plugins\nayden-import\js\index.js"></script>
-        <?php
-        $variable_products = isset($_POST['variable_products']) ? true : false;
-
-        $data = json_decode(file_get_contents('php://input'), true);
-        echo "Data from php POST: ";
-        print_r($data);
-        // $selectedColumn = $data['column'];
-
-        // // Read CSV file and fetch rows associated with the selected column
-        // $allLines = [];
-        // $handle = fopen($file, 'r');
-        // while (($line = fgetcsv($handle)) !== FALSE) {
-        //     // Assuming the first row contains column headers
-        //     $columnIndex = array_search($selectedColumn, $line);
-        //     if ($columnIndex !== false) {
-        //         // Add only rows that have the selected column
-        //         $allLines[] = $line;
-        //     }
-        // }
-        // fclose($handle);
+        // Close the file handle
+        fclose($handle);
     }
 }
-if(!defined('ABSPATH')){
-    echo 'What are you trying to do?';
-    exit();
+
+function receiving_js_data(){
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        
+        $json_data = file_get_contents('php://input');
+        // print_r($json_data);
+        $data_field_body_arr = json_decode($json_data, true); // true parameter converts it to associative array
+        // Check if the 'data' key exists in the decoded data
+        $product_args = array();
+        print_r($data_field_body_arr);
+        if (isset($data_field_body_arr)) {
+            foreach ($data_field_body_arr as $key => $value) {
+                  
+                // Trim keys and values
+                $key = trim($key);
+                $value = trim($value);
+                $product_args[$key] = $value;
+                echo "Key: " . $key . ", Value: " . $value . "\n";
+            }
+            $product_args['post_type'] = 'product';
+            print_r($product_args);
+            $product_id = wp_insert_post($product_args);
+            update_post_meta($product_id, '_product_type', 'simple');
+
+            // Set product status to 'publish'  
+            wp_update_post(array('ID' => $product_id, 'post_status' => 'publish'));
+
+            // Set product category (replace 'category-slug' with the actual category slug)
+            wp_set_post_terms($product_id, 'Fashion', 'product_cat');
+
+            // Set product tags (replace 'tag1, tag2' with actual tag names)
+            wp_set_post_terms($product_id, 'tag1, tag2', 'product_tag');
+        }
+    }
 }
+// if(!defined('ABSPATH')){
+//     echo 'What are you trying to do?';
+//     exit();
+// }
+function delete_products(){
+    for($i = 1707163835; $i <= 1707163836; $i++){
+        if (get_post_type($i) === 'product') {
+            // Delete the product
+            wp_delete_post($i, true); // Set the second parameter to true to force delete
+            echo 'Product deleted successfully.';
+        } else {
+            echo 'Product does not exist.';
+        }
+    }
+}
+function my_plugin_scripts() {
+    wp_enqueue_script('my-plugin-script', plugins_url('..\nayden-import\js\index.js', __FILE__), array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'my_plugin_scripts');
+
 add_action( 'admin_menu', 'product_importer_menu' );
 add_action( 'admin_init', 'process_product_import' );
+receiving_js_data();
+// delete_products();
